301fb5a60b92d9d620facba69f6eb3dc16d9de13
Kati <mayssketii@gmail.com>
2025-03-31 06:42:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        double tiketprice = 20000;
+        int rublesformila = 15;
+
+        int bonusmiles = (int) (tiketprice / rublesformila);
+
+        System.out.println(bonusmiles);
+    }
+}
