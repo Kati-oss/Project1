4fef0ef3d5f1d7aa642e3198dbb6ae55eb4b7199
Kati <mayssketii@gmail.com>
2025-04-01 13:15:47 +0300

    src/main.java

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        double tiketprice = 20000;
-        int rublesformila = 15;
+        double tiketPrice = 20000;
+        int rublesFormila = 15;
 
-        int bonusmiles = (int) (tiketprice / rublesformila);
+        int bonusMiles = (int) (tiketPrice / rublesFormila);
 
-        System.out.println(bonusmiles);
+        System.out.println(bonusMiles);
     }
 }
